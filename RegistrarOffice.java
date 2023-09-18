5a58db60c9675dbb834b557a6ef69388142b7f8c
Fernando Romo <faromo0214@gmail.com>
2023-09-18 15:16:24 -0700

    New file

---
+++ RegistrarOffice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class RegistrarOffice {
+    public static void main(String[] args) {
+        //each student has a unique student ID, and each course has a unique course code
+        HashMap<Integer,Integer> students = new HashMap<Integer,Integer>();
+        HashMap<Integer,Integer> courses = new HashMap<Integer,Integer>();
+
+        
+        
+
+    }
+    
+}
